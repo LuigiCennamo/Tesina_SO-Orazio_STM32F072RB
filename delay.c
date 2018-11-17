9b2206e4bead6e05dcd286d4fcffddde383905ed
SimonePonzio <simone.ponzio.94@gmail.com>
2018-11-17 13:52:34 +0100

    modified:   delay.c

--- delay.c
+++ delay.c
@@ -3,17 +3,15 @@
 #include "stm32f0xx_nucleo.h"
 
 void delayMs(uint16_t ms){
-    /*ABILITAZIONE LINEE CLOCK*/
-
-	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;		// abilitazione clock linea gpio PA5
+	__disable_irq();
 	RCC->APB1ENR |= RCC_APB1ENR_TIM7EN;		// abilitazione clock linea timer TIM7
 
-	/*SETTAGGIO TIMER7*/
+	// SETTAGGIO TIMER7
 
 	TIM7->ARR = ms;
 	TIM7->PSC |= 0xBB80;
 	
-	/*ABILITO CONTATORE*/
+	// ABILITO CONTATORE
 
 	TIM7->CR1 |= TIM_CR1_CEN;
 	
@@ -22,4 +20,5 @@ void delayMs(uint16_t ms){
 	
 	TIM7->CR1 &= ~TIM_CR1_CEN; //disabilito contatore
 	RCC->APB1ENR &= ~RCC_APB1ENR_TIM7EN;		// disabilito clock linea timer TIM7
+	__enable_irq();
 }
